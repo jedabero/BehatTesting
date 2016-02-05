bf54e42827badca02f172ca3cad340d4ae31c572
jedabero <jedabero@hotmail.com>
2016-02-05 08:45:30 -0500

    Implemented scenario in feature context

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -19,6 +19,9 @@ use Behat\Gherkin\Node\PyStringNode,
  */
 class FeatureContext extends BehatContext
 {
+
+    private $output;
+
     /**
      * Initializes context.
      * Every scenario gets its own context object.
@@ -30,15 +33,44 @@ class FeatureContext extends BehatContext
         // Initialize your context here
     }
 
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
+    /**
+     * @Given /^I am in a directory "([^"]*)"$/
+     */
+    public function iAmInADirectory($dir)
+    {
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
+        chdir($dir);
+    }
+
+    /**
+     * @Given /^I have a file named "([^"]*)"$/
+     */
+    public function iHaveAFileNamed($file)
+    {
+        touch($file);
+    }
+
+    /**
+     * @When /^I run "([^"]*)"$/
+     */
+    public function iRun($command)
+    {
+        exec($command, $output);
+        $this->output = trim(implode("\n", $output));
+    }
+
+    /**
+     * @Then /^I should get:$/
+     */
+    public function iShouldGet(PyStringNode $string)
+    {
+        if ((string) $string != $this->output) {
+            throw new Exception("Actual output is: \n" . $this->output);
+        }
+    }
+
+
+
 }
